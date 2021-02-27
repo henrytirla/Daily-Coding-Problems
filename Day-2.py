d2e7a9c11bb2e66c6465e6db9d4397531c827c02
Henry Tirla <Henrytirla@gmail.com>
2021-02-27 01:41:32 +0100

    Question FOr Day2

--- Day-2.py
+++ Day-2.py
@@ -15,3 +15,16 @@ The following test should pass:
 
 node = Node('root', Node('left', Node('left.left')), Node('right'))
 assert deserialize(serialize(node)).left.left.val == 'left.left'"""
+#
+# Serialization is the process of converting a
+# data structure or object into a sequence of bits
+# so that it can be stored in a file or memory buffer,
+# or transmitted across a network connection
+# link to be reconstructed later
+# in the same or another computer environment.
+class Node:
+
+     pass
+
+
+
