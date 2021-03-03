eb71bf69c4849b3bafe19f0b89ccc05bdd51107e
Henry Tirla <Henrytirla@gmail.com>
2021-03-03 01:24:11 +0100

    Q2 update

--- Day-2.py
+++ Day-2.py
@@ -1,30 +1,11 @@
-"""This problem was asked by Google.
-
-Given the root to a binary tree, implement serialize(root),
-which serializes the tree into a string, and deserialize(s),
-which deserializes the string back into the tree.
-
-For example, given the following Node class
-
-class Node:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-The following test should pass:
-
-node = Node('root', Node('left', Node('left.left')), Node('right'))
-assert deserialize(serialize(node)).left.left.val == 'left.left'"""
-#
-# Serialization is the process of converting a
-# data structure or object into a sequence of bits
-# so that it can be stored in a file or memory buffer,
-# or transmitted across a network connection
-# link to be reconstructed later
-# in the same or another computer environment.
-class Node:
-
-     pass
+# Get product of all other elements
+# Given an array of integers, return a new array such that each element at index i of
+# the new array is the product of all the numbers in the original array except the one
+# at i.
+# For example, if our input was [ 1, 2, 3, 4, 5], the expected output would be [ 120,
+# 60, 40, 30, 24]. Ifourinputwas [3, 2, 1],theexpectedoutputwouldbe [2,
+# 3, 6].
+# Follow-up: What if you can't use division?
 
 
 
