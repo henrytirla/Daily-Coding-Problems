111e9a92ba9ca7127a0e16cf59636bd42845941c
Henry Tirla <Henrytirla@gmail.com>
2021-03-03 03:10:17 +0100

    Q2 update

--- Day-2.py
+++ Day-2.py
@@ -3,9 +3,22 @@
 # the new array is the product of all the numbers in the original array except the one
 # at i.
 # For example, if our input was [ 1, 2, 3, 4, 5], the expected output would be [ 120,
-# 60, 40, 30, 24]. Ifourinputwas [3, 2, 1],theexpectedoutputwouldbe [2,
+# 60, 40, 30, 24]. If ourinputwas [3, 2, 1],theexpectedoutputwouldbe [2,
 # 3, 6].
 # Follow-up: What if you can't use division?
+
+def products(num_array):
+   pass
+
+    #grrr Working on solution
+
+
+
+
+
+print(products([3,2,1]))
+
+
 
 
 
