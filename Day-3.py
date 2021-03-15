834204c1113e06610b0d0e2c1afc8e02984bcb23
Henry Tirla <Henrytirla@gmail.com>
2021-03-15 22:58:51 +0100

    Q3 (On) time

--- Day-3.py
+++ Day-3.py
@@ -2,6 +2,25 @@
 # window that must be sorted in order for the entire array to be sorted. For example,
 # given [ 3 , 7 , 5 , 6 , 9] , you should return ( 1 , 3 ) .
 
-##Understand What is meant by out of order
-## Bounds ?
-##Solve After Software testing assignment
+
+
+def window(array):
+        left, right= None, None
+        n = len(array)
+        max_seen, min_seen = -float("inf"), float("inf")
+        for i in range(n):
+            max_seen = max(max_seen, array[i])
+            if array[i] < max_seen:
+                right = i
+        for i in range(n - 1, -1, -1):
+            min_seen = min(min_seen, array[i])
+            if array[i] > min_seen:
+                left = i
+
+        return left, right
+
+
+
+
+
+print(window([3 , 7 , 5 , 6 , 9]))
