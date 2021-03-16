2952a80aea198127fd7670ea6faf78e23573477f
Henry Tirla <Henrytirla@gmail.com>
2021-03-16 23:28:26 +0100

    bruteforce approach

--- Day-4.py
+++ Day-4.py
@@ -8,4 +8,16 @@ take any elements.
 Do this in O ( n) time.
 Follow-up: What if the elements can wrap around? For example, given [ 8, -1, 3,
 4], return 15, as we choose the numbers 3, 4, and 8 where the 8 is obtained from
-wrapping around."""
+wrapping around.
+eg 3,-2,5,-1
+"""
+
+def Maxsubarray(array):
+    max_sub = 0
+    for i in range(len(array)-1):
+        for j in range(i,len(array)):
+            max_sub= max(max_sub,sum(array[i:j]))
+    return max_sub
+
+
+print(Maxsubarray([3,-2,5,-1]))
