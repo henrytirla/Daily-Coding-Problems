8692e3da7d3041fde3a2c6e7758b9d67b083c7e0
Henry Tirla <Henrytirla@gmail.com>
2021-03-17 23:04:52 +0100

    Question 5

--- Day-2.py
+++ Day-2.py
@@ -3,7 +3,7 @@
 # the new array is the product of all the numbers in the original array except the one
 # at i.
 # For example, if our input was [ 1, 2, 3, 4, 5], the expected output would be [ 120,
-# 60, 40, 30, 24]. If our input was [3, 2, 1],theexpectedcoutput would be [2,
+# 60, 40, 30, 24]. If our input was [3, 2, 1],the expectedcoutput would be [2,
 # 3, 6].
 # Follow-up: What if you can't use division?
 
--- Day-5.py
+++ Day-5.py
@@ -1,11 +1,11 @@
 """Given an array of integers, return a new array where each element in the new array
 is the number of smaller elements to the right of that element in the original input
 array.
-For example, given the array [
-return [
+For example, given the array [3, 4, 9, 6, 1],
+return [1, 1, 2, 1, 0],
 since:
-3, 4, 9, 6, 1],
-1, 1, 2, 1, 0],
+
+
 • There is 1 smaller element to the right of 3
 • There is 1 smaller element to the right of 4
 • There are 2 smaller elements to the right of 9
