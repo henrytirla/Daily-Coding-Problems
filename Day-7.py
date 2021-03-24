c2b75df44ee1b58b8e2784e067f77de89222119f
Henry Tirla <Henrytirla@gmail.com>
2021-03-24 22:45:31 +0100

    Q_7

---
+++ Day-7.py
@@ -0,0 +1,4 @@
+"""Given a list of words, find all pairs of unique indices such that the concatenation of
+the two words is a palindrome.
+For example, given the list [ 11 code 11,11 edoc 11,11 da 11,11 d 11],
+return [ ( 0, 1), ( 1,0), (2, 3)]."""
