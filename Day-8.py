21b61a2713fc8c2eb32810feaab4a13260bd2bf9
Henry Tirla <Henrytirla@gmail.com>
2021-03-26 23:45:17 +0100

    Q_8

---
+++ Day-8.py
@@ -0,0 +1,3 @@
+"""Given a string and a number of lines k, print the string in zigzag form. In zigzag,
+characters are printed out diagonally from top left to bottom right until reaching
+the k th line, then back up to top right, and so on."""
