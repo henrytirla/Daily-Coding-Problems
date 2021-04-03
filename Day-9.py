428e912d75df6258a42b9c0baa5d3c34879dce25
Henry Tirla <Henrytirla@gmail.com>
2021-04-03 23:12:54 +0200

    Q_8

---
+++ Day-9.py
@@ -0,0 +1,10 @@
+"""You are given a string of length n and an integer k. The string can be manipulated
+by taking one of the first k letters and moving it to the end of the string.
+Write a program to determine the lexicographically smallest string that can be created
+after an unlimited number of moves.
+
+For example, suppose we are given the string da i 1 y and k = l. The best we can
+create in this case is ailyd.
+
+
+"""
