b6e4b58706466344050946cfb6de09dda9eb3dff
henrytirla <henrytirla@gmail.com>
2021-10-11 16:10:47 +0100

    sample output added

--- Day-8.py
+++ Day-8.py
@@ -1,3 +1,14 @@
 """Given a string and a number of lines k, print the string in zigzag form. In zigzag,
 characters are printed out diagonally from top left to bottom right until reaching
-the k th line, then back up to top right, and so on."""
+the k th line, then back up to top right, and so on.
+
+EG Thisisazigzag and k=4
+
+ t       a        g
+  h     s z     a
+    i  i   i   z
+      s      g
+
+
+"""
+
