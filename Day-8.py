ee0fd020d91f32626823018170eb62bacb94a881
henrytirla <henrytirla@gmail.com>
2021-10-13 18:48:31 +0100

    Day 8 completed and solution explained

--- Day-8.py
+++ Day-8.py
@@ -10,5 +10,51 @@ EG Thisisazigzag and k=4
       s      g
 
 
+
+Solution Explanation Medium: https://henrytirla.medium.com/day-8-print-a-string-in-zigzag-form-in-k-rows-python-9ec5c05351cf
+In case of any questiions leave a comment
 """
 
+def printZigZag(s, k):
+ space =""
+ # base case
+ if k == 0:
+  return
+
+ # base case
+ if k == 1:
+  print(s, end='')
+  return
+
+ # print first row
+ for i in range(0, len(s), (k - 1) * 2):
+  print(s[i], end='')
+
+
+ # print middle rows
+ for j in range(1, k - 1):
+
+  down = True
+  i = j
+
+  while i < len(s):
+   print("down", s[i], end='')
+   if down:  # going down
+    i += (k - j - 1) * 2
+
+   else:  # going up
+    i += (k - 1) * 2 - (k - j - 1) * 2
+
+
+   down = not down  # switch direction
+
+ # print last row
+ for i in range(k - 1, len(s), (k - 1) * 2):
+  print(s[i], end='')
+
+
+if __name__ == '__main__':
+ s = 'Thisisazigzag'
+ k = 4
+
+ printZigZag(s, k)
