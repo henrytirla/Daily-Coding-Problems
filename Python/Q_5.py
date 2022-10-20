082e9d46a5503923841f82ca12f139b91efeedd7
Henry Tirla <henrytirla@gmail.com>
2022-10-21 00:32:58 +0100

    Q5 useful resource

--- Python/Q_5.py
+++ Python/Q_5.py
@@ -1,6 +1,8 @@
 """This problem was asked by Google.
 
-Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+Given the root to a binary tree, implement serialize(root),
+which serializes the tree into a string, and deserialize(s),
+which deserializes the string back into the tree.
 
 For example, given the following Node class
 
@@ -12,4 +14,15 @@ class Node:
 The following test should pass:
 
 node = Node('root', Node('left', Node('left.left')), Node('right'))
-assert deserialize(serialize(node)).left.left.val == 'left.left'"""
+assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+##Definition
+
+Serialization is the process of converting a data structure or object into a sequence of bits
+so that it can be stroed in a file or memory buffer, or transmitted across a network connection
+link to be reconstructed later in the same or another computer environment
+
+#useful resource
+https://www.youtube.com/watch?v=Qtf8ieq3zco
+"""
+
