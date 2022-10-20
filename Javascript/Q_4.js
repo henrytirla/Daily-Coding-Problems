8bacac8055d150e90d4893ad02accd49df7fd4f1
Henry Tirla <henrytirla@gmail.com>
2022-10-20 18:06:52 +0100

    Q_4

--- Javascript/Q_4.js
+++ Javascript/Q_4.js
@@ -10,13 +10,55 @@ Follow-up: what if you can't use division?
 
 
 **/
-function ProductSum(){
-  var prefix_array=[];
-  for (let i=0; i< arr.length; i++){
-     console.log(i);
-  }
+function ProductSum(arr){
+    var prefix_array=[];
+    for (let i=0; i< arr.length; i++){
+        if (prefix_array.length != 0){
+            prefix_array.push(prefix_array[prefix_array.length-1]* arr[i]);
+
+        }
+        else{
+            prefix_array.push(arr[i]);
+
+        }
+    }
+    var suffix_array =[];
+    var new_arr = arr.reverse();
+    for (let i=0; i< new_arr.length; i++){
+        if(suffix_array.length != 0){
+            suffix_array.push(suffix_array[suffix_array.length-1] * new_arr[i] )}
+        else{
+            suffix_array.push(new_arr[i])
+        }
+
+    }
+    suffix_array = suffix_array.reverse()
+    //console.log(prefix_array)
+    //console.log(suffix_array)
+
+    var result = [];
+
+    for (let i =0; i<arr.length; i++){
+
+        if (i == 0){
+
+            result.push(suffix_array[i+1])
+
+        }
+        else if(i == arr.length -1){
+            result.push(prefix_array[i-1])
+        }
+        else{
+            result.push(prefix_array[i-1] * suffix_array[i+1])
+        }
 
 
-//var arr=[1,2,3,4];
+    }
+    return result
+}
 
-console.log(ProductSum())
+var arr=[3,2,1];
+
+console.log(ProductSum(arr))
+
+
