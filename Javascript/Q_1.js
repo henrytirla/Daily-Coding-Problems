b1a2654dd44e89936e0fafd1174912373b9710b2
Henry Tirla <henrytirla@gmail.com>
2022-10-20 18:17:48 +0100

    fixed 1

--- Javascript/Q_1.js
+++ Javascript/Q_1.js
@@ -5,9 +5,7 @@ the function should return them in an array, in any order. If no two numbers sum
 target sum, the function should return an empty array
 
 
-Sample Input
-array = [5, 1, 22, 25, 6, -1, 8, 10]
-sequence = [1, 6, -1, 10]
+
 ***/
 var array = [5, 1, 22, 25, 6, -1, 8, 10];
 var targetSum= 23;
