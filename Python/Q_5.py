1274ec6bf1d36a95a03330934b737c98316edb02
Henry Tirla <henrytirla@gmail.com>
2022-10-21 01:10:30 +0100

    Q5

--- Python/Q_5.py
+++ Python/Q_5.py
@@ -27,3 +27,21 @@ https://www.youtube.com/watch?v=Qtf8ieq3zco
 https://www.youtube.com/watch?v=u4JAi2JJhI8
 """
 
+def serialize(root):
+    if root is None:
+        return '#'
+    return '{} {} {}'.format(root.val, serialize(root.left), serialize(root.right))
+
+def deserialize(data):
+    def helper():
+        val = next(vals)
+        if val == '#':
+            return None
+        node = Node(int(val))
+        node.left = helper()
+        node.right = helper()
+        return node
+    vals = iter(data.split())
+    return helper()
+
+
