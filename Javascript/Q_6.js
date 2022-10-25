ce2a06a440c548e48dd872f4bcc6b1b95414fce9
Henry Tirla <henrytirla@gmail.com>
2022-10-25 22:31:07 +0100

    Q_6 Js Solution

--- Javascript/Q_6.js
+++ Javascript/Q_6.js
@@ -27,3 +27,44 @@
 
  **/
 
+function checkAnagram(s,t){
+    if (s.length != t.length){
+        return false
+    }
+    var countS = new Map();
+    var countT = new Map();
+
+    for (i=0; i< s.length;i++){
+
+        if(countS.size != 0 && countS.has(s[i])){
+
+            countS = countS.set(s[i],1+ countS.get(s[i]));
+
+        }
+        else{
+            countS = countS.set(s[i],1);}
+
+        if( countT.size != 0 && countT.has(t[i])){
+            countT = countT.set(t[i],1+ countT.get(t[i]));
+        }else{
+
+            countT = countT.set(t[i],1);}
+    }
+     // console.log("S",countS)
+     // console.log("T",countT)
+    for (const key of countS.keys()){
+        if (countS.get(key) != countT.get(key) ){
+            return  false
+        }
+     }
+    return  true
+
+
+}
+s ="anagram"
+
+t="nagaran"
+
+console.log(checkAnagram(s,t))
+
+
