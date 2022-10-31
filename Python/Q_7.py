4e629da6f0e674f15d244261f22604a7dffff43b
Henry Tirla <henrytirla@gmail.com>
2022-10-31 21:10:19 +0000

    Q_7 solution

--- Python/Q_7.py
+++ Python/Q_7.py
@@ -2,7 +2,8 @@
 
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+An Anagram is a word or phrase formed by rearranging the letters of a
+different word or phrase, typically using all the original letters exactly once.
 
 
 
@@ -31,5 +32,47 @@ strs[i] consists of lowercase English letters.
 
 Useful resources
 https://leetcode.com/problems/group-anagrams/
-https://neetcode.io/practice
+
+
+Solution Explanation
+https://www.youtube.com/embed/vzdNOK2oB2E
+
+"Understand ORD fucntion and what it does"
+https://www.youtube.com/watch?v=NfwArODGWHE
+
+"Understand Defautl Dict"
+https://www.geeksforgeeks.org/defaultdict-in-python/
 """
+
+from collections import defaultdict
+
+str = ["eat","tea","tan","ate","nat","bat"]
+
+
+def GroupAnagram(str):
+    res = defaultdict(list)
+    for s in str:
+        count= [0] * 26
+        for c in s:
+            count[ord(c)-ord("a")] +=1
+        res[tuple(count)].append(s)
+    return res.values()
+print(GroupAnagram(str))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
