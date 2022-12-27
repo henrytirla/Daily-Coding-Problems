d83cfb2de836142778396ab79b7cb3a4c2e86d13
Henry Tirla <henrytirla@gmail.com>
2022-12-27 21:22:15 +0000

    Q_13 Solution

--- Python/Q_13.py
+++ Python/Q_13.py
@@ -20,3 +20,49 @@ Sample Output
 
 
 """
+
+def find_closest_pair(arr1,arr2):
+    arr1.sort()
+    arr2.sort()
+
+    min_diff = float('inf')
+    closest_pair=[]
+
+    i=0
+    j=0
+
+    while  i < len(arr1) and  j < len(arr2):
+
+          diff = abs(arr1[i] -arr2[j])
+
+          if diff < min_diff:
+               min_diff = diff
+               closest_pair=[arr1[i],arr2[j]]
+
+          if arr1[i] < arr2[j]:
+              i += 1
+
+          else:
+              j += 1
+
+    return closest_pair
+
+
+arrayOne = [-1, 5, 10, 20, 28, 3]
+arrayTwo = [26, 134, 135, 15, 17]
+
+print(find_closest_pair(arrayOne,arrayTwo))
+
+
+"""
+This algorithm first sorts both input arrays, and then uses two pointers (i and j) to iterate over 
+the arrays simultaneously. It calculates the absolute difference between the elements at the pointers 
+and updates the minimum difference and the closest pair as needed. It also moves the pointers in the 
+appropriate direction (either to the right or to the left) to continue comparing the elements.
+
+The time complexity of this algorithm is O(n log n), because the time complexity of the sorting algorithm (O(n log n)) 
+is dominant over the time complexity of the two-pointer approach (O(n)).
+
+
+"""
+
