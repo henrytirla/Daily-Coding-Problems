09cf6f9b068ac2c138c9666d2e050f948dad0d4f
Henry Tirla <henrytirla@gmail.com>
2022-12-27 13:50:56 +0000

    Q 12

--- Python/Q_12.py
+++ Python/Q_12.py
@@ -18,3 +18,52 @@ Sample Output
 [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
 
 """
+
+def find_tripplets(array, targetSum):
+
+
+   array.sort()
+   tripplet=[]
+
+
+   for i in range(len(array)):
+       L = i +1
+       R = len(array)-1
+       while L<R:
+         sum = array[i]+ array[L] + array[R]
+         if sum == targetSum:
+            tripplet.append([array[i], array[L] ,array[R]])
+            L += 1
+            R -= 1
+
+         elif sum < targetSum:
+            L +=1
+         else :
+            R-=1
+   return tripplet
+
+arr= [12, 3, 1, 2, -6, 5, -8, 6]
+
+targetSum =0
+
+print(find_tripplets(arr,targetSum))
+
+
+
+"""O(n)3 Solution
+In this case, the time complexity is O(n^3) because the number of iterations of the outermost loop
+(which has a time complexity of O(n)) is multiplied by the number of iterations of the middle loop 
+(which also has a time complexity of O(n)), which is in turn multiplied by the number of iterations of the innermost loop
+ (also O(n)). This results in a total time complexity of O(n^3).
+
+
+
+"""
+# def find_triplets(arr, target_sum):
+#     triplets = []
+#     for i in range(len(arr)):
+#         for j in range(i+1, len(arr)):
+#             for k in range(j+1, len(arr)):
+#                 if arr[i] + arr[j] + arr[k] == target_sum:
+#                     triplets.append([arr[i], arr[j], arr[k]])
+#     return triplets
