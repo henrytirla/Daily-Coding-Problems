47d2e3ee4a4cf5d69261f72a8a48670d27b426f9
Henry Tirla <henrytirla@gmail.com>
2023-01-06 23:46:05 +0000

    done

--- Python/Q_16.py
+++ Python/Q_16.py
@@ -14,29 +14,6 @@ array = [
 ]
 Sample Output
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]"""
-
-# def spiralTraverse(array):
-#     result=[]
-#     startRow, endRow =0, len(array)-1
-#     startCol, endCol =0, len(array[0])-1
-#
-#     while startRow <= endRow and startCol<= endCol:
-#         for col in range(startCol,endCol+1):
-#             result.append(array[startRow][col])
-#         for row in range(startRow +1, endRow+1):
-#             result.append(array[startRow][endCol])
-#         for col in reversed(range(startCol,endCol)):
-#             result.append(array[endRow][col])
-#         for row in reversed(range(startRow+1, endRow)):
-#             result.append(array[row][startCol])
-#         startRow+=1
-#         endRow-=1
-#         startCol+=1
-#         endCol-=1
-#
-#     return result
-
-
 # O(n) time | O(n) space - where n is the total number of elements in the array
 def spiralTraverse(array):
     result = []
