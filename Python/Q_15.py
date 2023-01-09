751d81e62afe8c83946df89a1337b21016cb478d
Henry Tirla <henrytirla@gmail.com>
2023-01-09 07:28:59 +0000

    revised for clarity

--- Python/Q_15.py
+++ Python/Q_15.py
@@ -17,14 +17,26 @@ Sample Output
 true
 
 
-Algorith
 
-Put a pointer on last array
-and on start
 
-while R<L:
- compare both pointer if L > R
- return False
+Note:
+Non-decreasing means that no element is less than the element before it
+Non-increasing means not becoming progressively greater
+
+Example to fully comprehend unecessary convoluted question grammar
+eg  array =[1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10, 11]
+
+It is not monotonic.
+
+A monotonic array is an array in which the elements either strictly increase or strictly decrease as we go through the array from left to right.
+ In this array, the elements do not have a consistent increasing or decreasing pattern.
+
+For example, the element 2 is followed by the element 3, which is higher, but then the element 4 is followed by the element 5, which is also higher.
+The element 5 is followed by two more elements 5, and then the element 6 is followed by the element 7.
+After the element 8, the element 7 is followed by the element 9, which is higher.
+
+Therefore, this array is not monotonic.
+
 
 
 
@@ -38,7 +50,7 @@ def isMonotonic(arr):
     if len(arr) <= 1:
         return True
 
-    # Check if the array is non-decreasing
+    # Check if the array is non-decreasing or non-Increasing
     nonDecreasing = True
     nonIncreasing = True
 
@@ -53,6 +65,8 @@ def isMonotonic(arr):
 
     return nonIncreasing or nonDecreasing
 
-array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+#array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+
+array= [1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10, 11]
 print(isMonotonic(array))
 
