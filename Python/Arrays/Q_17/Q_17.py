14eb836f2938374175bb40c406472eeca5157040
Henry Tirla <henrytirla@gmail.com>
2023-02-10 16:48:59 +0000

    Restructured questions to include testing

---
+++ Python/Arrays/Q_17/Q_17.py
@@ -0,0 +1,79 @@
+"""Write a function that takes in an array of integers and returns the length of the longest peak in the array.
+
+A peak is defined as adjacent integers in the array that are strictly increasing until they reach a tip
+ (the highest value in the peak), at which point they become strictly decreasing. At least three integers are required to form a peak.
+
+For example, the integers 1, 4, 10, 2 form a peak, but the integers 4, 0, 10
+don't and neither do the integers 1, 2, 2, 0.
+Similarly, the integers 1, 2, 3 don't form a peak because
+there aren't any strictly decreasing integers after the 3.
+
+Sample Input
+array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+Sample Output
+6 // 0, 10, 6, 5, -1, -3
+
+Hint 1
+You can solve this question by iterating through the array from left to right once.
+
+Hint 2
+Iterate through the array from left to right, and treat every integer as the potential
+tip of a peak. To be the tip of a peak, an integer has to be strictly greater than its adjacent integers.
+What can you do when you find an actual tip?
+
+Hint 3
+As you iterate through the array from left to right, whenever you find a tip of a peak,
+expand outwards from the tip until you no longer have a peak.
+Given what peaks look like and how many peaks can therefore fit in an array,
+realize that this process results in a linear-time algorithm.
+Make sure to keep track of the longest peak you find as you iterate through the array.
+
+
+Optimal Space & Time Complexity
+O(n) time | O(1) space - where n is the length of the input array
+
+ALgorithm
+-Start with two pointers at extremities
+-If right pointer < left pointer mover right pointer else move left pointer
+
+-if left pointer >
+
+
+"""
+
+# def longest_peak(arr):
+#     leftIdx =0
+#     rightIdx= len(arr)-1
+#
+#     for i in range(len(arr)):
+#         while rightIdx > leftIdx:
+#               a=arr[leftIdx]
+#               b= arr[rightIdx]
+#               if arr[leftIdx]>arr[rightIdx]:
+#                  rightIdx-=1
+#               elif arr[leftIdx]== arr[rightIdx]:
+#                    rightIdx-=1
+#               elif arr[leftIdx] < arr[rightIdx]:
+#                    leftIdx +=1
+#         return [arr[leftIdx-1],arr[leftIdx],arr[leftIdx+1]]
+#
+#
+# arr= [1,2,3,5,6,4,0]
+# arr2=[1,2,3,3,4,0,10,6,5,-1,-3,2,3]
+# arr3= [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
+# print(longest_peak(arr3))
+
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+# Create an instance of the Car class
+my_car = Car("Toyota", "Camry", 2020)
+
+# Access the instance variables
+print(my_car.make)
+print(my_car.model)
+print(my_car.year)
