8975285e957ab5199269528fbbf7ce8e9fc46f71
Henry Tirla <henrytirla@gmail.com>
2023-03-27 08:40:29 +0100

    Q_18

--- Python/Arrays/Q_18/Q_18.py
+++ Python/Arrays/Q_18/Q_18.py
@@ -18,3 +18,73 @@ Sample Output
 
 
 """
+
+arr=[5,1,4,2]
+
+# Copyright © 2022 AlgoExpert LLC. All rights reserved.
+
+# O(n) time | O(n) space - where n is the length of the input array
+def arrayOfProducts(array):
+    products = [1 for _ in range(len(array))]
+
+
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        products[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        products[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+
+
+
+    return products
+
+print(arrayOfProducts(arr))
+
+
+
+
+
+# def arrayOfProducts(array):
+#     # create result array with size array
+#     results = [0] * len(array)
+#     # set first index to all values ahead, set index to 1
+#     # and pass in value we don't want to calculate in index
+#     # 0 forward, since indices ahead need this value
+#     results[0] = array_helper(array, 1, results, array[0])
+#     return results
+#
+#
+# def array_helper(array, index, results, value):
+#     # base case, if index is equal to array length then return 1
+#     if index == len(array):
+#         return 1
+#     # val will always eqaul the multplication of all indices ahead of
+#     # current index, in the recursive call increment index, and pass in
+#     # value, which will be the multplication of all values before the current
+#     # index, multiplied by the current index
+#     val = array_helper(array, index + 1, results, array[index] * value)
+#     # we set the current index to all past multiplied values * all feature
+#     # multiplied values
+#     results[index] = value * val
+#     # then we return all feature multiplied values to the previous call
+#     # this requires we multiply the current value at index * all the
+#     # feature multiplied values we have received ahead of this index
+#     return array[index] * val
+
+# new_arr=[]
+# product= [1 for _ in range(len(arr))]
+# for i in range(len(arr)):
+#     runningProduct= 1
+#     for j in range(len(arr)):
+#         if i!=j:
+#             runningProduct *= arr[j]
+#     product[i]= runningProduct
+#
+#
+#
+# print(product)
+
---
+++ Python/Arrays/Q_18/test_Q_18.py
@@ -0,0 +1,5 @@
+from Q_18 import arrayOfProducts
+
+def test_arrayOfProducts():
+    arr = [5, 1, 4, 2]
+    assert  arrayOfProducts(arr) == [8, 40, 10, 20]
