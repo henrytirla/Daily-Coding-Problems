efdea6556a6ac85aec6cd32f4681a8db78342e21
Henry Tirla <henrytirla@gmail.com>
2023-05-21 02:20:49 +0100

    Q_29

--- Python/Arrays/Q_29/Q_29.py
+++ Python/Arrays/Q_29/Q_29.py
@@ -21,41 +21,4 @@ Sample Output
 2  // [1, 1], [0, 0], [0, 1], and [1, 0] makes a square,
 // as does [1, 1], [-4, 2], [-2, -1], and [-1, 4]"""
 
-def zigZag(array):
-    height= len(array)-1
-    width = len(array[0])
-    result = []
-    row,col = 0,0
-    goingDown = True
-    while not isOutOfBounds(row,col,height,width):
-              if goingDown:
-                  if col==0 and row == height:
-                      goingDown= False
-                      if row == height:
-                         col+=1
-                      else:
-                         row+=1
-                  else:
-                      row+=1
-                      col-=1
-                  if row==0 and col == width:
-                      goingDown = True
-                      if col == width:
-                         row+=1
-                      else:
-                          col+=1
-                  else:
-                      row-=1
-                      col+=1
-    return result
 
-
-
-
-
-
-
-
-
-def isOutOfBounds(array):
-    return row< 0 or row>height or col <0 or col>height
