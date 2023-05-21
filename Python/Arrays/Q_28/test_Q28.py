682f3724d4376b43e98790e2c9b33a435938788f
Henry Tirla <henrytirla@gmail.com>
2023-05-21 02:19:14 +0100

    Q_29

---
+++ Python/Arrays/Q_28/test_Q28.py
@@ -0,0 +1,12 @@
+from Q_28 import zigzagTraverse
+
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+def test_zigzagTraverse():
+    #TestCase1
+    array = [
+        [1, 3, 4, 10],
+        [2, 5, 9, 11],
+        [6, 8, 12, 15],
+        [7, 13, 14, 16],]
+    assert(zigzagTraverse(array)) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
