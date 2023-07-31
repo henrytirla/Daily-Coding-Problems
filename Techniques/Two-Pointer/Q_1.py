9bdf814b53b1f4d4dffb78dacbd80e72e5d64b74
Henry Tirla <henrytirla@gmail.com>
2023-07-31 16:07:17 +0200

    Added code for execution visualization

--- Techniques/Two-Pointer/Q_1.py
+++ Techniques/Two-Pointer/Q_1.py
@@ -64,3 +64,31 @@ class Solution:
 
 sol= Solution()
 print(sol.mergeSort(nums1,3,nums2,3))
+
+
+
+"""
+Appreciate code execution visualization on PythonTutor.com
+def mergeSort(num1,m,num2,n):
+            last = m + n-1
+    
+            while m > 0 and n > 0:
+                if num1[m-1] > num2[n-1]:
+                    num1[last]= num1[m-1]
+                    m-=1
+                else:
+                    num1[last] = num2[n-1]
+                    n -=1
+                last -=1
+    
+            while n>0:
+                num1[last] = num2[n-1]
+                n, last = n-1 , last -1
+            return num1
+    
+    
+    
+    
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]    
+mergeSort(nums1,3,nums2,3)"""
