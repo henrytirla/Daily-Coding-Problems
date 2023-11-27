7b5aa8c5bcda797e29ac1995795e178d806af245
Henry Tirla <henrytirla@gmail.com>
2023-11-27 16:22:36 +0100

    Q_2 Problems Solving with loops

--- Techniques/Problems Solving With Loops/Q_2.py
+++ Techniques/Problems Solving With Loops/Q_2.py
@@ -6,3 +6,28 @@ The largest element of an array is also a leader by default.
 Suppose all the array elements are unique.
 Ordering in the output doesn’t matter.
 """
+
+def test_find_leaders():
+    sol= Solution()
+    arr = [16, 17, 4, 3, 5, 2],
+
+    Output: [17, 5, 2]
+    assert sol.find_leaders([16, 17, 4, 3, 5, 2]) ==[2,5,17]
+
+class Solution:
+    def find_leaders(self,arr:list[int])-> int:
+        leaders = []
+        max_so_far = -1
+        for i in range(len(arr) - 1, -1, -1):
+            if arr[i] > max_so_far:
+                leaders.append(arr[i])
+                max_so_far = arr[i]
+        return leaders
+
+
+
+if __name__ == '__main__':
+    sol= Solution()
+    arr=[16, 17, 4, 3, 5, 2]
+
+    print(sol.find_leaders(arr))
