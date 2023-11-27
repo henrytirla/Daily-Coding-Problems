f572f9af31cb67a820ac4f114eadf11078d43fab
Henry Tirla <henrytirla@gmail.com>
2023-11-27 13:32:26 +0100

    Q_2 Problems Solving with loops

---
+++ Techniques/Problems Solving With Loops/Q_2.py
@@ -0,0 +1,8 @@
+"""Given an integer array X[] of size n, write a program to find all the leaders in the array X[].
+An element is a leader if it is strictly greater than all the elements to its right side.
+
+The last element of an array is a leader by default.
+The largest element of an array is also a leader by default.
+Suppose all the array elements are unique.
+Ordering in the output doesn’t matter.
+"""
