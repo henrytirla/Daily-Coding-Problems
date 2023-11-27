dff8f8d78bb99b1ebbe0541a2f069a6ddc68feb7
Henry Tirla <henrytirla@gmail.com>
2023-11-27 10:01:23 +0100

    Q_1 Problems Solving with loops

---
+++ Techniques/Problems Solving With Loops/Q_1.py
@@ -0,0 +1,7 @@
+"""Given an input array height[] representing the heights of buildings, write a program to count the number of buildings facing the sunset. It is assumed that the heights of all buildings are distinct.
+
+Examples
+Input: height[] = [7, 4, 8, 2, 9], Output: 3 Explanation: As 7 is the first element, it can see the sunset. Similarly, 8 and 9 can see the sunset. 4 and 2 can't see the sunset because 7 and 8 are hiding it.
+
+Input: height[] = [2, 3, 4, 5], Output: 4"""
+
