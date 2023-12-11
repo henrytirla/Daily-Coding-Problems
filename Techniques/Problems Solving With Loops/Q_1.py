afe9e00c1a89ef395c276002348342612a20288f
Henry Tirla <henrytirla@gmail.com>
2023-12-11 11:24:47 +0100

    Q_1 Problems Solving with loops

--- Techniques/Problems Solving With Loops/Q_1.py
+++ Techniques/Problems Solving With Loops/Q_1.py
@@ -5,8 +5,16 @@ Input: height[] = [7, 4, 8, 2, 9], Output: 3 Explanation: As 7 is the first elem
 
 Input: height[] = [2, 3, 4, 5], Output: 4
 
-"""
+credits EnjoyAlgorithms.com
 
+"""
+def test_countBuilding():
+    sol = Solution()
+
+    # Testcase1
+    assert sol.count_Building([7, 4, 8, 2, 9]) == 3
+    # Testcase2
+    assert sol.count_Building([2, 3, 4, 5]) == 4
 
 class Solution():
 
@@ -22,22 +30,7 @@ class Solution():
 
 
 
-if __name__ == "__main__":
-    sol = Solution()
 
-    building_list = [7, 4, 8, 2, 9]
-
-    # Call the count_Building method and print the result
-    print("The Number of Buildings Facing the sun:", sol.count_Building(building_list))
-
-
-def test_countBuilding():
-    sol = Solution()
-
-    # Testcase1
-    assert sol.count_Building([7, 4, 8, 2, 9]) == 3
-    # Testcase2
-    assert sol.count_Building([2, 3, 4, 5]) == 4
 
 
 """"Critical ideas to think!
