56710113a1373f10a75d804127ca2ca92d7ba131
Henry Tirla <henrytirla@gmail.com>
2023-12-11 10:55:46 +0100

    Q_4 Problems Solving with loops

--- Techniques/Problems Solving With Loops/Q_4.py
+++ Techniques/Problems Solving With Loops/Q_4.py
@@ -15,4 +15,23 @@ A number in Roman numerals is a string of these symbols written in descending or
 We avoid four characters being repeated in successions such as IIII or VIIII. Instead, the number four is written as IV because the 1 is before the 5, and we subtract it making 4. The same principle applies to number 9, which is written as IX. The idea is: when there is a smaller number placed before a larger number, the values are subtracted.
 
 
+
+Example 1: Input: XL, Output: 40
+
+Explanation: XL is a Roman symbol which represents 40
+
+Example 2: Input: MCMIV, Output: 1904
 """
+
+def test_roman_Decimal():
+    sol= Solution()
+
+    assert sol.roman_Decimal("XL") == 40
+    assert sol.roman_Decimal("LX")== 60
+
+
+class Solution:
+    def roman_Decimal(self,romanstring:str)-> int:
+        roman_dict={"I": 1,"V":5,"X":10,"L":50, "C":100,"D":500,"M":1000}
+
+        pass
