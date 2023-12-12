e76fcf10f2dac0398fdfda2ae4d5093cdbbe97cc
Henry Tirla <henrytirla@gmail.com>
2023-12-12 12:42:11 +0100

    Q_6 Problems Solving with loops

---
+++ Techniques/Problems Solving With Loops/Q_6.py
@@ -0,0 +1,6 @@
+"""Given an array X[] of n integers where n > 1, write a program to return an array product[] such that product[i] is equal to the product of all the elements of X except X[i].
+
+It's guaranteed that the product of the elements of any prefix or suffix of the array (including the entire array) fits into a 32-bit integer.
+We need to solve this problem without using division operations.
+
+"""
