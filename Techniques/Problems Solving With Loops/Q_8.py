4de1ca2d7f54ea16a5b7c0a211bbc0c74f80aa69
Henry Tirla <henrytirla@gmail.com>
2023-12-13 12:26:09 +0100

    Q_8 Problems Solving with loops

---
+++ Techniques/Problems Solving With Loops/Q_8.py
@@ -0,0 +1,22 @@
+"""Given a 2-dimensional m x n matrix, print all the elements in spiral order.
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+
+
+"""
+
+
+class Solution:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+
+        pass
