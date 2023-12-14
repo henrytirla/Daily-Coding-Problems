ad2aa37da1b9b59d5710cf82a167043ad0a60d5e
Henry Tirla <henrytirla@gmail.com>
2023-12-14 13:27:32 +0100

    Q_9 Problems Solving with loops

---
+++ Techniques/Problems Solving With Loops/Q_9.py
@@ -0,0 +1,6 @@
+"""Given an array X[] of size n, we need to find the maximum and minimum elements present in the array. Our algorithm should make the minimum number of comparisons.
+
+Examples
+Input: X[] = [4, 2, 0, 8, 20, 9, 2], Output: max = 20, min = 0
+
+Input: X[] = [-8, -3, -10, -32, -1], Output: max = -1, min = -32"""
